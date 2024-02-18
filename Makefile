4d38896b92fd697f215cf8e70983a4cf353910e6
Saptarshi Sengupta <saptarshisg96@gmail.com>
2024-02-18 18:22:10 +0530

    inital build

---
+++ Makefile
@@ -0,0 +1,13 @@
+help:
+	@echo "Available commands"
+
+setup:
+	@echo "Setting up development environment"
+	npm install
+
+deploy:
+	npm install
+	npm run build
+	rm -rf node_modules/
+	npm install --omit=dev
+	npm publish
---
+++ gulpfile.js
@@ -0,0 +1,40 @@
+const fs = require('fs')
+const fsExtra = require('fs-extra')
+const gulp = require('gulp')
+const swc = require('gulp-swc')
+const terser = require('gulp-terser')
+
+const swcOptions = {
+  jsc: {
+    target: 'es5'
+  },
+  module: {
+    type: 'commonjs',
+  },
+}
+
+const clear = function () {
+  return fsExtra.remove('dist')
+}
+
+const generatePackageJson = function () {
+  const packageJson = {
+    'type': 'commonjs'
+  }
+  fs.writeFileSync('dist/package.json', JSON.stringify(packageJson, null, 2))
+  return Promise.resolve()
+}
+
+const buildProject = function () {
+  return gulp.src('src/**/*.ts')
+    .pipe(swc(swcOptions))
+    .pipe(terser({
+      mangle: true, // Enable variable name mangling
+      compress: {
+        unused: true, // Remove unused code
+      },
+    }))
+    .pipe(gulp.dest('dist'))
+}
+
+exports.build = gulp.series([clear, buildProject, generatePackageJson])
